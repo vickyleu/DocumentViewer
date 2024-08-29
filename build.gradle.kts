958f33a2657dbb3b0f7a6f7ad837d7cab0090a85
vicky leu <yu6564172@gmail.com>
2024-08-29 10:15:40 +0800

    first commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,12 +35,12 @@ plugins {
     alias(libs.plugins.jetbrains.compose).apply(false)
     alias(libs.plugins.compose.compiler).apply(false)
 
-    id(libs.plugins.android.application.get().pluginId).apply(false) //.get().pluginId
-    id(libs.plugins.android.library.get().pluginId).apply(false)
-    id(libs.plugins.kotlin.multiplatform.get().pluginId).apply(false)
-//    alias(libs.plugins.android.application).apply(false) //.get().pluginId
-//    alias(libs.plugins.android.library).apply(false)
-//    alias(libs.plugins.kotlin.multiplatform).apply(false)
+//    id(libs.plugins.android.application.get().pluginId).apply(false) //.get().pluginId
+//    id(libs.plugins.android.library.get().pluginId).apply(false)
+//    id(libs.plugins.kotlin.multiplatform.get().pluginId).apply(false)
+    alias(libs.plugins.android.application).apply(false) //.get().pluginId
+    alias(libs.plugins.android.library).apply(false)
+    alias(libs.plugins.kotlin.multiplatform).apply(false)
 
     alias(libs.plugins.dokka)
     id("maven-publish")
