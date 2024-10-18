f3a2ea7574b7e1dc3a36d5319bdb4f6e890f1880
vicky leu <yu6564172@gmail.com>
2024-10-19 03:29:41 +0800

    first commit

--- viewer/src/androidMain/kotlin/org/uooc/document/DocumentView.kt
+++ viewer/src/androidMain/kotlin/org/uooc/document/DocumentView.kt
@@ -1,9 +1,11 @@
 package org.uooc.document
 
 import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.AttributeSet
 import android.util.Log
+import android.view.ViewTreeObserver
 import android.widget.FrameLayout
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
@@ -16,6 +18,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
+import kotlin.math.roundToInt
 
 private val TAG = "DocumentPreviewer"
 
@@ -53,6 +56,8 @@ class DocumentView @JvmOverloads constructor(
                 //文件格式
                 val fileExt = FileUtils.getFileType(file.toString())
                 println("文件格式：$fileExt")
+
+
                 withContext(Dispatchers.Main) {
                     val bool = TbsFileInterfaceImpl.canOpenFileExt(fileExt)
                     Log.d(TAG, "文件是否支持$bool  文件路径：$file $bsReaderTemp $fileExt")
@@ -62,31 +67,55 @@ class DocumentView @JvmOverloads constructor(
                         localBundle.putString("filePath", file.absolutePath.toString())
                         localBundle.putString("tempPath", bsReaderTemp)
                         localBundle.putString("fileExt", fileExt)
+                        localBundle.putBoolean("file_reader_stream_mode", true)//设置为文件流打开模式
                         localBundle.putInt(
                             "set_content_view_width",
-                            with(density) { width.toFloat().dp.roundToPx() })
-                        val ret = TbsFileInterfaceImpl.getInstance().openFileReader(
-                            context, localBundle,
-                            { code, args, msg ->
-                                Log.e(TAG, "文件打开回调 $code  $args  $msg")
-                                when (code) {
-                                    ITbsReader.NOTIFY_CANDISPLAY -> {
-                                        //文件即将显示
-                                        Log.wtf("NOTIFY_CANDISPLAY", "文件即将显示")
+                            with(density) { width.toFloat().dp.value.roundToInt() })
+                        // TODO  高度目前还不确定需不需要,等腾讯回复
+                        localBundle.putInt(
+                            "set_content_view_height",
+                            with(density) { height.toFloat().dp.value.roundToInt().coerceAtLeast(200) })
+                        this@DocumentView.post {
+                            val ret = TbsFileInterfaceImpl.getInstance().openFileReader(
+                                context, localBundle,
+                                { code, args, msg ->
+                                    Log.e(TAG, "文件打开回调 $code  $args  $msg")
+                                    when (code) {
+                                        ITbsReader.OPEN_FILEREADER_STATUS_UI_CALLBACK->{
+                                            if (args is Bundle) {
+                                                val id = args.getInt("typeId", 0)
+                                                val typeDes = args.getString("typeDes", "fileReaderOpened")
+                                                if (ITbsReader.TBS_READER_TYPE_STATUS_UI_OPENED == id) {
+                                                    //加密文档弹框取消需关闭activity
+//                                                Navigation.findNavController(getView()).popBackStack()
+                                                    if(completer.isCompleted.not()){
+                                                        completer.complete(false to "文件打开失败:${msg}")
+                                                    }
+                                                } else if (ITbsReader.TBS_READER_TYPE_STATUS_UI_SHUTDOWN == id) {
+                                                    //加密文档弹框取消需关闭activity
+//                                                Navigation.findNavController(getView()).popBackStack()
+                                                    if(completer.isCompleted.not()){
+                                                        completer.complete(false to "文件打开失败:${msg}")
+                                                    }
+                                                }
+                                            }else{
+                                                if(completer.isCompleted.not()){
+                                                    completer.complete(false to "文件打开失败:${msg}")
+                                                }
+                                            }
+                                        }
+                                        ITbsReader.NOTIFY_CANDISPLAY -> {
+                                            //文件即将显示
+                                            Log.wtf("NOTIFY_CANDISPLAY", "文件即将显示")
+                                            completer.complete(true to "")
+                                        }
                                     }
-                                }
-                                if (args is Bundle) {
-                                    val id = args.getInt("typeId", 0)
-                                    if (ITbsReader.TBS_READER_TYPE_STATUS_UI_SHUTDOWN == id) {
-                                        //加密文档弹框取消需关闭activity
-                                    }
-                                }
-                            }, this@DocumentView
-                        )
-                        if (ret == 0) {
-                            completer.complete(true to "")
-                        } else {
-                            completer.complete(false to "error:$ret")
+                                }, this@DocumentView
+                            )
+                            if (ret == 0) {
+                            } else {
+                                completer.complete(false to "error:$ret")
+                            }
                         }
                     } else {
                         Log.e(TAG, "文件打开失败！文件格式暂不支持")
@@ -99,8 +128,21 @@ class DocumentView @JvmOverloads constructor(
         callback.invoke(rlt, msg)
     }
 
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        this.getViewTreeObserver().addOnGlobalLayoutListener(object :
+            ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                this@DocumentView.getViewTreeObserver().removeOnGlobalLayoutListener(this)
+                val w: Int = this@DocumentView.width
+                val h: Int = this@DocumentView.height
+                TbsFileInterfaceImpl.getInstance().onSizeChanged(w, h)
+            }
+        })
+    }
     fun dispose() {
         try {
+            this.removeAllViews()
             val instance = TbsFileInterfaceImpl.getInstance()
             instance.closeFileReader()
         } catch (ignore: Exception) {
